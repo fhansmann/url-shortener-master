0fabdc43fa9a03730ab97c33a5a9b3691bee89fb
Florian Hansmann <hansmann.f@gmail.com>
2020-11-03 09:51:50 +0100

    update

---
+++ server/routes/url.routes.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const urlRoutes = express.Router()
+
+const controller = require("../controllers/url.controller")
+
+urlRoutes.get("/:slug", controller.getUrl)
+
+urlRoutes.post("/new", controller.postUrl)
+
+module.exports = urlRoutes
